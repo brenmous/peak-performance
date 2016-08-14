8172d8f32719976e1e08311cc768ae293ab78d8d
Sdevv <sowmya.devarakonda@griffithuni.edu.au>
2016-08-14 20:32:30 +1000

    Initial Dream collection view commit

---
+++ PeakPerformance/DreamCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  DreamCollectionViewCell.swift
+//  PeakPerformance
+//
+//  Created by Sai on 13/08/2016.
+//  Copyright © 2016 derridale. All rights reserved.
+//
+
+import UIKit
+
+class DreamCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cellImg: UIImageView!
+    
+    @IBOutlet weak var dreamDesc: UILabel!
+}
