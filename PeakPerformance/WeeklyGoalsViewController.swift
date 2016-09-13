0628b72b709f97e6e8ee47b00ab6cbf55e176776
Benjamin Chiong <Bjchiong@computer-001.local>
2016-09-14 01:54:51 +1000

    burger icon badge notification in weeklygoalviewcontroller added. Burger icon update for all views.

--- PeakPerformance/WeeklyGoalsViewController.swift
+++ PeakPerformance/WeeklyGoalsViewController.swift
@@ -23,6 +23,10 @@ class WeeklyGoalsViewController: UITableViewController, WeeklyGoalDetailViewCont
     /// This view controller's data service.
     let dataService = DataService( )
     
+    /// Declare leftBarButton badge with counter
+    var leftBarButton: ENMBadgedBarButtonItem?
+    var count = 0
+    
     // MARK: Outlets
     
     //progress bar
@@ -196,6 +200,7 @@ class WeeklyGoalsViewController: UITableViewController, WeeklyGoalDetailViewCont
         super.viewDidLoad()
         
         DateTracker( ).getCurrentYearAsString()
+        setUpLeftBarButtonItem( )
     
     }
  
@@ -378,5 +383,40 @@ class WeeklyGoalsViewController: UITableViewController, WeeklyGoalDetailViewCont
         }
         
     }
+
+    // MARK: - Menu Notification Badge
+    //  - Loads the burger icon with the badge if a monthly review is available
+    //  - Sets up a UIImage and a Highlighted UIImage and a button and assigns it to leftBarButtonItem
+    //  - ENMBadgedBarButtonItem is responsible for the badge
     
+    func setUpLeftBarButtonItem( ) {
+        
+        let image = UIImage(named: "menu-150dpi-2")
+        let highlightedImage = UIImage(named: "menu-150dpi-highlighted")
+        let button = UIButton(type: .Custom)
+        
+        if let knownImage = image {
+            button.frame = CGRectMake(0.0, 0.0, knownImage.size.width, knownImage.size.height)
+        } else {
+            button.frame = CGRectZero;
+        }
+        
+        button.setBackgroundImage(image, forState: UIControlState.Normal)
+        button.setBackgroundImage(highlightedImage, forState: .Highlighted)
+        button.addTarget(self,
+                         action: #selector(leftButtonPressed(_:)),
+                         forControlEvents: UIControlEvents.TouchUpInside)
+        button.adjustsImageWhenHighlighted = true
+        button.tintColor = UIColor.lightGrayColor()
+        let newBarButton = ENMBadgedBarButtonItem(customView: button, value: "1") // parameter value for the number inside the dot
+        leftBarButton = newBarButton
+        leftBarButton?.badgeValue = "1"  // sets the dot and the number inside
+        navigationItem.leftBarButtonItem = leftBarButton
+    }
+    
+    // function to BarButton item tap
+    func leftButtonPressed(_sender: UIButton) {
+        presentViewController(SideMenuManager.menuLeftNavigationController!, animated: true, completion: nil)
+        navigationItem.leftBarButtonItem?.tintColor = UIColor.lightGrayColor()
+    }
 }
