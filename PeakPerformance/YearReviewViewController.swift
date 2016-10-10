dab907a87da1a47d4179f266847a03085ee48682
Bren <Bren@ryan.local>
2016-10-10 22:10:48 +1000

    disable editing of yearly summaries

--- PeakPerformance/YearReviewViewController.swift
+++ PeakPerformance/YearReviewViewController.swift
@@ -18,6 +18,9 @@ class YearReviewViewController: UITableViewController {
     /// DataService instance for interacting with Firebase database.
     let dataService = DataService()
     
+    /// The yearly summary being dealt with.
+    var summary: YearlySummary?
+    
     // MARK: - Outlets
     @IBOutlet weak var reasonsForDifferencesTextView: UITextView!
     
@@ -28,8 +31,9 @@ class YearReviewViewController: UITableViewController {
     // MARK: - Actions
     @IBAction func doneButtonPressed(sender: AnyObject)
     {
-        guard let summary = currentUser!.yearlySummary[currentUser!.year] else { return }
-        summary!.reviewed = true
+        //guard let summary = currentUser!.yearlySummary[currentUser!.year] else { return }
+        guard let summary = self.summary else { return }
+        summary.reviewed = true
         self.yearlyCleanUp()
         self.performSegueWithIdentifier(UNWIND_TO_HISTORY_SEGUE, sender: self)
     }
@@ -38,13 +42,37 @@ class YearReviewViewController: UITableViewController {
     /// Resets user's monthly reviews/summaries, gets new summaries for current 12 month period. Sets yearly summary to nil.
     func yearlyCleanUp()
     {
-        guard let summary = currentUser!.yearlySummary[currentUser!.year] else { return }
+        //guard let summary = currentUser!.yearlySummary[currentUser!.year] else { return }
+        guard let summary = self.summary else { return }
         //Save the completed yearly summary
-        self.dataService.saveYearlySummary(self.currentUser!, summary: summary!)
+        self.dataService.saveYearlySummary(self.currentUser!, summary: summary)
         
         //Update user's year property
         let yearsPassedSinceStart = NSDate().checkTwelveMonthPeriod(self.currentUser!)
         self.currentUser!.year = yearsPassedSinceStart
         self.dataService.saveUserYear(self.currentUser!)
     }
+    
+    // MARK: - Overriden methods
+    override func viewDidLoad()
+    {
+        guard let summary = self.summary else { return }
+        //If the summary has been reviwed, hide the graphic, place summary text in fields and disable/hide "Done" button.
+        if summary.reviewed
+        {
+            reasonsForDifferencesTextView.text = summary.reasonsForDifferencesText
+            observedAboutMyPerformanceTextView.text = summary.observedAboutPerformanceText
+            changedMyPerformance.text = summary.changedMyPerformanceText
+            reasonsForDifferencesTextView.editable = false
+            observedAboutMyPerformanceTextView.editable = false
+            changedMyPerformance.editable = false
+            
+            let doneButton = self.navigationItem.rightBarButtonItem
+            doneButton?.title = ""
+            doneButton?.enabled = false
+            
+        }
+    }
+    
+
 }
